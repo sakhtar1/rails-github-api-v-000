ad33efd8d70d2c38329ada88894dd60e003c46ff
Sharmina Akhtar <sakht318@gmail.com>
2018-09-18 19:57:33 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,13 +3,13 @@ class RepositoriesController < ApplicationController
    user_response = Faraday.get("https://api/github.com/user") do |req|
      req.headers["Authorization"] = "token" + session[:token]
      req.headers["Accept"] = 'application/json'
-   end 
+   end
    @user = JSON.parse(user_response.body)
-# req.params instead of headers? syntax used in other lessons 
+# req.params instead of headers? syntax used in other lessons
     repo_response = Faraday.get("https://api.github.com/user/repos") do |req|
      req.params["Authorization"] = "token" + session[:token]
      req.params["Accept"] = 'application/json'
-   end 
+   end
    @repos = JSON.parse(repo_response.body)
   end	  end
 end
