b57eca19129761aa6025e84cc96c12fd1bfacfe4
Sharmina Akhtar <sakht318@gmail.com>
2018-09-18 19:57:22 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     client_secret =  = ENV["GITHUB_CLIENT_SECRET"]
     code = params["code"]
  # this is #2 in the flow
-#  what's the diff between req.params and req.body and req.headers 
+#  what's the diff between req.params and req.body and req.headers
     response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code' :code } 
-      # req.params 
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code' :code }
+      # req.params
        req.headers['Accept'] = 'application/json'
     end
     body = parse.JSON(response.body)
