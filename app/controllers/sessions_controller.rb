582a6ce217ff063fcdb9053cc0e9d3f63b7d395f
Sharmina Akhtar <sakht318@gmail.com>
2018-09-18 19:57:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,16 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    client_id = ENV["GITHUB_CLIENT_ID"]
-    client_secret =  = ENV["GITHUB_CLIENT_SECRET"]
-    code = params["code"]
- # this is #2 in the flow
-#  what's the diff between req.params and req.body and req.headers
-    response = Faraday.post("https://github.com/login/oauth/access_token") do |req|
-      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code' :code }
-      # req.params
-       req.headers['Accept'] = 'application/json'
-    end
-    body = parse.JSON(response.body)
-    session[:token] = body["access_token"]
-    redirect_to root_path
-  end	  end
+    resp = Faraday.post "https://github.com/login/oauth/access_token" do |req|
+      client_id = ENV['GITHUB_CLIENT_ID']
+      client_secret = ENV['GITHUB_SECRET']
+      code = params['code']
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.headers['Accept'] = 'application/json'
+      redirect_uri="http://localhost:3000/auth"
+   end
+   body = JSON.parse(resp.body)
+   session[:token] = body["access_token"]
+   redirect_to root_path
+ end
 end
